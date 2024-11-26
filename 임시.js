052f5122a2d9028817858400903237ed024a17d2
magnoria <sidaumv@gmail.com>
2024-11-26 18:23:19 +0900

    11월26일백업

---
+++ 임시.js
@@ -0,0 +1,18 @@
+
+
+let array = [11, 22, 33]
+
+console.log(array)
+
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+
+
+
+let 연습 = <ul>
+            <li>연습1</li>
+            <li>연습2</li>
+            <li>연습3</li>
+
+</ul>
